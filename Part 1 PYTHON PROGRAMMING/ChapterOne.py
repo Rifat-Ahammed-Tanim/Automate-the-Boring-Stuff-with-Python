6a3f3ad771446cbe068199f50a01d48da4ec2813
Rifat Ahammed Tanim <138393708+Rifat-Ahammed-Tanim@users.noreply.github.com>
2025-08-26 21:06:06 +0600

    first commit

---
+++ Part 1 PYTHON PROGRAMMING/ChapterOne.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators :-)
+
+# Exponent **
+a = 2
+b = 3
+print(a ** b)
+
+# Modulus %
+a = 22
+b = 8
+print(a % b)
+
+# Integer division //
+a = 22
+b = 8
+print(a // b)
+
+# Division
+a = 22
+b = 8
+print(a / b)
+
+# Multiplication *
+a = 2
+b = 10
+print(a * b)
+
+# Subtraction -
+a = 100
+b = 50
+print(a - b)
+
+# Addition +
+a = 18
+b = 12
+print(a + b)
