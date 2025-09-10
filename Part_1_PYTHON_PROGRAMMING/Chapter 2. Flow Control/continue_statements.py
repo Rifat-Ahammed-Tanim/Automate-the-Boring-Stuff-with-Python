77f85e66510421f3eaf3d1ff5230b3b6d6a6d910
Rifat Ahammed Tanim <138393708+Rifat-Ahammed-Tanim@users.noreply.github.com>
2025-09-10 23:19:55 +0600

    add some statements

---
+++ Part_1_PYTHON_PROGRAMMING/Chapter 2. Flow Control/continue_statements.py
@@ -0,0 +1,9 @@
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Tanim':
+        continue
+    print('Hello, Tanim. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'ilisha':
+        break
---
+++ Part_1_PYTHON_PROGRAMMING/Chapter 2. Flow Control/while_loop.py
@@ -0,0 +1,27 @@
+# Example 1
+# spam = 0
+# if spam < 5:
+#     print("Hello, world.")
+#     spam = spam + 1
+
+
+# Example 2
+# name = ""
+# while name != "Tanim":
+#     print("Please type your name.")
+#     name = input()
+# print("Thank you!")
+
+
+# Example 3
+# while True:
+#     print('Please type your name.')
+#     name = input()
+#     if name == 'Tanim':
+#         break
+# print('Thank you!')
+
+#Example of Infinite Loop
+# while True:
+#     print('Please type your name.')
+
